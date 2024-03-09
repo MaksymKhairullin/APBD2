801e4bb5732809f3e33bf124957de22061a3aeaa
s22884 <s22884@pjwstk.edu.pl>
2024-03-09 18:30:55 +0100

    Feature Average commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!!!!!!!");
-Console.WriteLine("My name is Max!!!");
-Console.WriteLine("Wow, it is github :) !!!");
+Console.WriteLine(GetAvg([1,2,3,4,5]));
+
+static double GetAvg(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum += arr[i];
+    }
+    return sum / arr.Length;
+}
