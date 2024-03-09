d4b3ce9aec7fe4b65f321237ee301ee2bbef77aa
s22884 <s22884@pjwstk.edu.pl>
2024-03-09 18:59:15 +0100

    Commit for conflict

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,16 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine(GetMax([1,2,3,4,5,7,123,45,32,12]));
+Console.WriteLine(GetAvg([1,2,3,4,5]));
 
-static int GetMax(int[] arr)
+static double GetAvg(int[] arr)
 {
-    int max = 0;
+    int sumNum = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i] > max)
-        {
-            max = arr[i];
-        }
+        sumNum += arr[i];
     }
-    return max;
+    return sumNum / arr.Length;
 }
