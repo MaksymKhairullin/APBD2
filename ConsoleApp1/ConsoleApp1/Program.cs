0e6bd744ffc80d8ec17de65e97042b778c026db6
s22884 <s22884@pjwstk.edu.pl>
2024-03-09 19:02:33 +0100

    Commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,16 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine(GetMax([1,2,3,4,5,7,123,45,32,12]));
+// See https://aka.ms/new-console-template for more information
 
-static int GetMax(int[] arr)
+Console.WriteLine(GetAvg([1,2,3,4,5]));
+
+static double GetAvg(int[] arr)
 {
-    int max = 0;
+    int sum = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i] > max)
-        {
-            max = arr[i];
-        }
+        sum += arr[i];
     }
-    return max;
+    return sum / arr.Length;
 }
